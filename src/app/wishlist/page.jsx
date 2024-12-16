30cb93eb33dfc13b58f36079f9cd5fb68f5f9e11
jashwant-jaladi <jasshwant.jvs@protonmail.com>
2024-12-16 14:59:29 +0530

    create wishlist schema

--- src/app/wishlist/page.jsx
+++ src/app/wishlist/page.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Wishlist = () => {
   return (
-    <div></div>
+    <div className="bg-[url('/about-1.avif')] h-40 bg-center bg-cover text-white font-bold grid place-content-center text-5xl border-b-2 border-yellow-400">
+    WISHLIST SECTION
+  </div>
+
+  
+
   )
 }
 
