7d366349d0bc10299f132060d2b099b343bba98c
jashwant-jaladi <jasshwant.jvs@protonmail.com>
2025-03-23 16:05:01 +0530

    deployment ready

--- src/app/categories/[category]/page.jsx
+++ src/app/categories/[category]/page.jsx
@@ -96,7 +96,7 @@ const CategoryPage = ({ params }) => {
   return (
     <div>
       <Shopnav onFilterChange={setSelectedFilter} />
-      <div className="bg-black text-slate-300 p-4 sm:p-14 pt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 sm:gap-14 place-items-center">
+      <div className="bg-black text-slate-300 p-4 sm:p-14 pt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 sm:gap-14 place-items-center">
         {filteredProducts.map((item) => (
           <Item
             key={item.id}
